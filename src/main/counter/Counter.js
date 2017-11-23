1a540b695619185849d18eb1757ab061d55027e8
Taras Shevchenko <Almonarx@gmail.com>
2017-11-23 17:16:23 +0200

    lesson

---
+++ src/main/counter/Counter.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export class Counter extends Component {
+    constructor() {
+        super();
+        this.counter = 0;
+    }
+
+    clickHandler = () => {
+        this.counter++;
+    };
+
+    render() {
+        return (
+            <div className="counter">
+                <button onClick={this.clickHandler}>Inc</button>
+                <span>{this.counter}</span>
+           </div>
+       );
+   }
+}
