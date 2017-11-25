b3d4318956ad9ce273fe3f9a61b9d43b2ba75f45
Taras Shevchenko <Almonarx@gmail.com>
2017-11-25 14:56:45 +0200

    25.11

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,18 +2,50 @@ import React, { Component } from 'react';
 import './main.scss';
 
 import { Numbers } from './numbers';
-import { StatusMessage } from "./statusMessage";
 import { Counter } from "./counter";
+import { Persons} from "./persons";
 
 export class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            loading: false
+        };
+    }
+
+    getUsers = () => {
+        this.setState({
+            loading: true,
+            users: []
+        });
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ users, loading: false }));
+    };
+
+    showUserInfo = (user) => {
+        alert(`${user.email}: ${user.phone}`);
+    };
+
     render() {
+        const { users, loading } = this.state;
+
         return (
             <main className="main">
-                    <p>Main</p>
-                    <Numbers from="2" to="9" even/>
-                    <StatusMessage />
-                    <StatusMessage valid/>
-                    <Counter />
+                <p>Main</p>
+                <Numbers from="2" to="9" even/>
+                <Counter />
+                <button onClick={this.getUsers}>
+                    Get users
+                </button>
+                <Persons
+                    users={users}
+                    clickHandler={this.showUserInfo}
+                />
+
+                { loading && <span>Loading...</span> }
             </main>
         );
     }
--- src/main/counter/Counter.js
+++ src/main/counter/Counter.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
 export class Counter extends Component {
-    constructor() {
-        super();
-        this.counter = 0;
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
     }
 
-    clickHandler = () => {
-        this.counter++;
-    };
+    clickHandler = () => this.setState({ counter: this.state.counter + 1 });
 
     render() {
         return (
             <div className="counter">
-                <button onClick={this.clickHandler}>Inc</button>
-                <span>{this.counter}</span>
+                <button onClick={this.clickHandler}>Inc</button>&nbsp;
+                <span>{this.state.counter}</span>
            </div>
        );
    }
